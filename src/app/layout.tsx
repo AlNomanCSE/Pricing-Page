834e8419502bcf41a00f26792f8eb4ea593089d4
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-03-12 15:17:08 +0600

    footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/app/header/Header";
-import Footer from "@/components/app/footer/footer";
+import Footer from "@/components/app/footer/Footer";
 
 const roboto = Roboto({
   subsets: ["latin"],
